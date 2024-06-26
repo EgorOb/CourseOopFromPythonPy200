35ff467fb7d551cf665550f5116d88e2da9398fd
Egor <egorobidin@inbox.ru>
2024-06-26 17:03:37 +0300

    1_4 done

--- Занятие 1. Основы ООП/Домашние задания/task7_Glass/task.py
+++
@@ -1,9 +0,0 @@
-from typing import Union
-
-
-# TODO  создать класс Glass
-
-
-if __name__ == "__main__":
-    glass = Glass(200, 100)  # экземпляр класса
-    print(glass.capacity_volume, glass.occupied_volume)
