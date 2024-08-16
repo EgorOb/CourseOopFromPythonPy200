ca0cb497c26d8320d57bbeaa7573a3d302d45cea
Egor <egorobidin@inbox.ru>
2024-08-16 15:18:22 +0300

    базовый курс

---
+++ Занятие 2. Атрибуты и методы/1. Практические задания/task7_Date/main.py
@@ -0,0 +1,32 @@
+class Date:
+    def __init__(self, day: int, month: int, year: int):
+        if not isinstance(day, int):
+            raise TypeError('Неверный тип day')
+        self.day = day
+        if not isinstance(month, int):
+            raise TypeError('Неверный тип month')
+        self.month = month
+        if not isinstance(year, int):
+            raise TypeError('Неверный тип year')
+        self.year = year
+
+    def __str__(self):
+        return f"{self.day:0>2}/{self.month:0>2}/{self.year:4}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(day={self.day!r}, month={self.month!r}, year={self.year!r})"
+
+
+if __name__ == "__main__":
+    date1 = Date(1, 1, 2021)
+    print(date1)  # 01/01/2021
+    date2 = Date(11, 10, 1999)
+    print(date2)  # 11/10/1999
+    print(repr(date1), repr(date2))  # Date(day=1, month=1, year=2021) Date(day=11, month=10, year=1999)
+
+    try:
+        Date('1', 1, 2021)
+    except TypeError:
+        print('Верный вызов TypeError')  # Верный вызов TypeError
+    else:
+        print('Должен быть вызов TypeError')
